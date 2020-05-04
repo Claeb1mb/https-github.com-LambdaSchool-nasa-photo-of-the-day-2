afbf65ff7a0cb3c70fbed512b09fa1043c7e276e
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-05-04 01:10:51 -0400

    Calling it a night get request not responding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+  //make a state for the data then set the state
+  //make a useEffect with a axios call with my chosen api
+  const [nasaData, setNasaData] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod")
+      .then((response) => {
+        console.log("Nasa Api Response", response);
+      })
+      .catch((error) => {
+        console.log("nasa api error", error);
+      });
+  });
+
+  return <div className="App"></div>;
+  //return the components
 }
 
 export default App;
+
+// Left off on
